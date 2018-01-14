f2c835448e03a052c14037f44eb693d408638ac7
ankitbajaj <ankitbajaj143@gmail.com>
2018-01-14 20:05:53 +0530

    second commit added util class

---
+++ src/main/java/com/example/infologlibrary/InfoUtilsClass.java
@@ -0,0 +1,15 @@
+package com.example.infologlibrary;
+
+import android.util.Log;
+
+/**
+ * Created by admin on 1/14/2018.
+ */
+
+public class InfoUtilsClass {
+
+    public static void LogMessage(String key, String value)
+    {
+        Log.i(key, value);
+    }
+}
